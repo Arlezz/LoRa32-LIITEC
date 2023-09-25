75dd1457af72a9f5ed90031d116e717b0615e2d1
Arlezz <antonyrodriguezc.2001@gmail.com>
2023-09-25 11:06:14 -0300

    uv sensor added

--- src/gyml8511Sensor.cpp
+++ src/gyml8511Sensor.cpp
@@ -5,49 +5,53 @@ gyml8511Sensor::gyml8511Sensor(LoraClient* client) : client(client) {
 }
 
 void gyml8511Sensor::gyml8511SensorSetup(){
-    OUT = 14;
-    EN_REF3V3 = 15;
+    OUT = 39;
+    EN_REF3V3 = 36;
 
     pinMode(OUT, INPUT);
-    pinMode(EN_REF3V3, OUTPUT);
+    pinMode(EN_REF3V3, INPUT);
 }
 
 int gyml8511Sensor::averageAnalogRead(int pinToRead)
 {
-    byte numberOfReadings = 8;
+    byte numberOfReadings = 10;
     unsigned int runningValue = 0; 
 
-    for(int x = 0 ; x < numberOfReadings ; x++)
+    for(int x = 0 ; x < numberOfReadings ; x++){
         runningValue += analogRead(pinToRead);
+    }
     runningValue /= numberOfReadings;
-
-    return(runningValue);
+  
+    return(runningValue);  
 }
 
 float gyml8511Sensor::mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void gyml8511Sensor::gyml8511SensorLoop() {
 
-    int uvLevel = averageAnalogRead(OUT);
+    float uvLevel = averageAnalogRead(OUT);
     int refLevel = averageAnalogRead(EN_REF3V3);
 
     //Utilice el pin de alimentación de 3,3 V como referencia para obtener un valor de salida muy preciso del sensor
     float outputVoltage = 3.3 / refLevel * uvLevel;
 
-    float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); //Convierte el voltaje en un nivel de intensidad UV
-
-    Serial.println("---------Sensor BM280----------");
-    Serial.print(F("UV = ")); 
-    Serial.print(uvIntensity);
-    Serial.println(" mW/cm^2");
+    float uvIntensity = mapfloat(outputVoltage, 0.87, 2.9, 0.0, 15.0); //Convierte el voltaje en un nivel de intensidad UV
 
     char uvFormatted[10];// Espacio suficiente para el formato
 
+
     // Formatear las variables con dos decimales
-    std::sprintf(uvFormatted, "%.2f", uvIntensity);
+    std::sprintf(uvFormatted, "%.2f", floor(uvLevel)/1000);
+
+    Serial.println("---------Sensor GYML8511--------");
+    Serial.print(" / uvLevel: ");
+    Serial.print(uvFormatted);
+    Serial.println("--------------------------------");
+
+
 
     (*client->sensorData)["UV"] = uvFormatted;
 }
